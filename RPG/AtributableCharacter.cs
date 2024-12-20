b14785465b13e99d87c42b28f7505270dc29610a
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-12-19 21:20:53 -0300

    Agrego Clases
    Agrego las clases iniciales para el proyecto

---
+++ RPG/AtributableCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    internal abstract class AtributableCharacter : Entity
+    {
+        public int EXP;
+        public int ATK;
+        public int SPD;
+        public int MAG;
+        public int DEF;
+    }
+}
---
+++ RPG/Entity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    abstract class Entity
+    {
+        public Texture2D texture;
+        public string name;
+        public int HP;
+    }
+}
