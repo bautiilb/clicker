39f936bb0362a71f3595e57ef3bb722e5377501a
chokoterisaki <47866874+chokoterisaki@users.noreply.github.com>
2024-12-21 16:19:38 -0300

    Agrego lógica de mapa por tiles

--- RPG/Game1.cs
+++ RPG/Game1.cs
@@ -1,18 +1,26 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RPG
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        protected GraphicsDeviceManager _graphics;
+        protected SpriteBatch _spriteBatch;
         PC player;
+        Tile tile;
+        List<Tile> tiles = new List<Tile>();
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            player = new PC("Player", 100, 0, 10, 10, 10, 10);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -20,16 +28,26 @@ namespace RPG
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            for (int i = 0; i <= 16; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    tiles.Add(new Tile(new Vector2(i, j), new Vector2(1,1)));
+                }
+            }
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            // Get the drawable area of the screen.
+            Viewport viewport = GraphicsDevice.Viewport;
             // TODO: use this.Content to load your game content here
-
+            foreach (Tile tile in tiles)
+            {
+                tile.LoadContent(Content, viewport);
+            }
             player.texture = Content.Load<Texture2D>("PC");
         }
 
@@ -39,7 +57,7 @@ namespace RPG
                 Exit();
 
             // TODO: Add your update logic here
-
+            
             base.Update(gameTime);
         }
 
@@ -47,7 +65,11 @@ namespace RPG
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            foreach(Tile tile in tiles)
+            {
+                tile.Draw(_spriteBatch);
+            }
+            
 
             base.Draw(gameTime);
         }
---
+++ RPG/Tile.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    internal class Tile
+    {
+        // Tiling Texture
+        private Texture2D texture;
+        // How many tiles wide
+        private int tileCountWidth = 8;
+        // How many tiles high
+        private int tileCountHeight = 8;
+        // Rectangle to draw tiles in
+        private Rectangle targetRectangle;
+        // Position to draw the tiled Rectangle at
+        private Vector2 t_position;
+        private Vector2 r_position;
+
+        public Tile(Vector2 relative_position, Vector2 type_position)
+        {
+            this.r_position = relative_position;
+            this.t_position = type_position;
+        }
+
+        public void LoadContent(ContentManager Content, Viewport viewport)
+        {
+            // Load the texture to tile.
+            texture = Content.Load<Texture2D>("Outside");
+
+            // Define a drawing rectangle based on the number of tiles wide and high, using the texture dimensions.
+            t_position = new Vector2(48 * Convert.ToInt32(t_position.X), 48 * Convert.ToInt32(t_position.Y));
+            targetRectangle = new Rectangle(0, 0, Convert.ToInt32(t_position.X) , Convert.ToInt32(t_position.Y) );
+
+
+            // Center the rectangle on the screen, using both the screen center and the rectangle center.
+            r_position = new Vector2(48 * Convert.ToInt32(r_position.X), 48 * Convert.ToInt32(r_position.Y));
+        }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.Opaque, SamplerState.LinearWrap, DepthStencilState.Default, RasterizerState.CullNone);
+            _spriteBatch.Draw(texture, r_position, targetRectangle, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+            _spriteBatch.End();
+        }
+    }
+}
